1d6cec26bab0ba67af8070f9847484357b1052be
Ch. König <criss@paars.de>
2018-08-06 11:00:43 +0200

    added docs to linkshell and force at least the record info if no files are specified

--- linkshell.go
+++ linkshell.go
@@ -5,8 +5,15 @@ import (
 	"net/url"
 )
 
+// Linkshell requests information about the provided linkshell ID
+// This call isn't implemented by XIVAPI yet, so this is pretty much a no-op call.
 func (c *XIVAPI) Linkshell(id uint64, files ...FileType) (interface{}, error) {
 	uri, _ := url.Parse(fmt.Sprintf("%v%v%v", BaseURL, "/Linkshell/", id))
+
+	// force record info
+	if len(files) == 0 {
+		files = append(files, FileRecord)
+	}
 
 	filesString := ""
 	for _, f := range files {
