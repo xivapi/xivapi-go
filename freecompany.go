48e843d55473cc8f3b5d17ac1578a12aaf454088
Adrian Zygadlo <adrian@azygadlo.com>
2018-09-19 10:50:43 -0400

    Added freecompany states and fc search

--- freecompany.go
+++ freecompany.go
@@ -5,10 +5,42 @@ import (
 	"net/url"
 )
 
+// FreeCompanyState : Shows current status of FreeCompany on XIVAPI
+type FreeCompanyState int
+
+const (
+	// StateNone : Content is not on XIVAPI and will not be added via this request
+	StateNone FreeCompanyState = iota
+	// StateAdding : Content does not exist on the API and needs adding.
+	StateAdding
+	// StateCached : Content exists in the system and you're being provided a cached response.
+	StateCached
+	// StateNotFound : Content does not exist on The Lodestone.
+	StateNotFound
+	// StateBlacklist : Content has been Blacklisted. No data can be obtained via the API for any application
+	StateBlacklist
+	// StatePrivate : Content is private on lodestone, ask the owner to make the content public and then try again!
+	StatePrivate
+)
+
+const freeCompanyEndpoint = "/FreeCompany/"
+
+type SearchFreeCompanyResult struct {
+	Pagination Pagination                    `json:"pagination"`
+	Results    []SearchFreeCompanyResultItem `json:"results"`
+}
+
+type SearchFreeCompanyResultItem struct {
+	Crest  []string `json:"Crest"`
+	ID     string   `json:"ID"`
+	Name   string   `json:"Name"`
+	Server string   `json:"Server"`
+}
+
 // FreeCompany requests information about the provided free company ID
 // This call isn't implemented by XIVAPI yet, so this is pretty much a no-op call.
 func (c *XIVAPI) FreeCompany(id uint64, files ...FileType) (interface{}, error) {
-	uri, _ := url.Parse(fmt.Sprintf("%v%v%v", BaseURL, "/FreeCompany/", id))
+	uri, _ := url.Parse(fmt.Sprintf("%v%v%v", BaseURL, freeCompanyEndpoint, id))
 
 	// force record info
 	if len(files) == 0 {
@@ -25,4 +57,30 @@ func (c *XIVAPI) FreeCompany(id uint64, files ...FileType) (interface{}, error) 
 	uri.RawQuery = values.Encode()
 
 	return nil, ErrNotImplemented
+}
+
+// FreeCompanySearch searches for a FreeCompany on XIVAPI provided a free company name
+func (c *XIVAPI) FreeCompanySearch(name, server, page string) (interface{}, error) {
+	if name == "" {
+		return nil, ErrMissingKey
+	}
+	uri, _ := url.Parse(fmt.Sprintf("%v%v%v", BaseURL, freeCompanyEndpoint, "Search"))
+
+	values := uri.Query()
+	values.Add("name", name)
+
+	if server != "" {
+		values.Add("server", server)
+	}
+	if page != "" {
+		values.Add("page", page)
+	}
+	uri.RawQuery = values.Encode()
+
+	r := new(SearchFreeCompanyResult)
+	if err := c.RequestJSON(MethodGet, uri, nil, r); err != nil {
+		return nil, err
+	}
+
+	return r, nil
 }
