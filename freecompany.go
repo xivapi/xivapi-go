7ba36b319c0ff742180f56aacfc69e5abd3b5f36
Adrian Zygadlo <adrian@azygadlo.com>
2018-09-21 14:56:14 -0400

    Adding the multitude of freecompany structs

--- freecompany.go
+++ freecompany.go
@@ -3,57 +3,15 @@ package xivapi
 import (
 	"fmt"
 	"net/url"
+	"strings"
 
 	"github.com/google/go-querystring/query"
 )
 
 const freeCompanyEndpoint = "/FreeCompany/"
 
-type SearchFreeCompanyResult struct {
-	Pagination Pagination                    `json:"pagination"`
-	Results    []SearchFreeCompanyResultItem `json:"results"`
-}
-
-type SearchFreeCompanyResultItem struct {
-	Crest  []string `json:"Crest"`
-	ID     string   `json:"ID"`
-	Name   string   `json:"Name"`
-	Server string   `json:"Server"`
-}
-
-//PlayerContentQueryOptions provide search parameters for all /*/Search endpoints (PvPTeam, Character, FreeCompany, LinkShell)
-type PlayerContentQueryOptions struct {
-	Name   string `url:"name,omitempty"`
-	Server string `url:"server,omitempty"`
-	Page   int    `url:"page,omitempty"`
-}
-
-// FreeCompany requests information about the provided free company ID
-func (c *XIVAPI) FreeCompany(id uint64, files ...FileType) (interface{}, error) {
-	uri, _ := url.Parse(fmt.Sprintf("%v%v%v", BaseURL, freeCompanyEndpoint, id))
-
-	// force record info
-	if len(files) == 0 {
-		files = append(files, FileRecord)
-	}
-
-	filesString := ""
-	for _, f := range files {
-		filesString += string(f) + ","
-	}
-
-	values := uri.Query()
-	values.Set("files", filesString[:len(filesString)-1])
-	uri.RawQuery = values.Encode()
-
-	return nil, ErrNotImplemented
-}
-
 // FreeCompanySearch searches for a FreeCompany on XIVAPI provided a free company name
 func (c *XIVAPI) FreeCompanySearch(name, server string, page int) (*SearchFreeCompanyResult, error) {
-	if name == "" {
-		return nil, ErrMissingKey
-	}
 	uri, _ := url.Parse(fmt.Sprintf("%v%v%v", BaseURL, freeCompanyEndpoint, "Search"))
 
 	opt := &PlayerContentQueryOptions{name, server, page}
@@ -66,4 +24,29 @@ func (c *XIVAPI) FreeCompanySearch(name, server string, page int) (*SearchFreeCo
 	}
 
 	return r, nil
+}
+
+// FreeCompany requests information about the provided free company ID
+func (c *XIVAPI) FreeCompany(id int64, isMembersIncluded bool, columns ...string) (*FreeCompanyResult, error) {
+	uri, _ := url.Parse(fmt.Sprintf("%v%v%v", BaseURL, freeCompanyEndpoint, id))
+	q := buildFreeCompanyQuery(isMembersIncluded, columns)
+	v, _ := query.Values(q)
+	uri.RawQuery = v.Encode()
+
+	r := new(FreeCompanyResult)
+	if err := c.RequestJSON(MethodGet, uri, nil, r); err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func buildFreeCompanyQuery(m bool, c []string) *FreeCompanyQuery {
+	r := new(FreeCompanyQuery)
+	r.Columns = strings.Join(c, ",")
+
+	if m {
+		r.Data = "FCM"
+	}
+
+	return r
 }
